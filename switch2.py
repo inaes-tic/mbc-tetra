881f37346364f5a9b7643443bb73a7f0357979c7
Adrian Pardini <publico@tangopardo.com.ar>
2013-06-16 16:47:00 -0300

    primer boceto algoritmo switcheo

--- switch2.py
+++ switch2.py
@@ -2,6 +2,9 @@
 
 import sys
 import time
+from collections import deque
+from itertools import ifilter
+
 import gi
 from gi.repository import GObject
 gi.require_version('Gst', '1.0')
@@ -14,7 +17,13 @@ GObject.threads_init()
 Gst.init(sys.argv)
 
 INPUT_COUNT = 3
-
+# seconds
+WINDOW_LENGTH = 1.5
+UPDATE_INTERVAL = .25
+MIN_ON_AIR_TIME = 3
+# dB
+NOISE_BASELINE = -45
+SPEAK_UP_THRESHOLD = 3
 
 class MainWindow(Gtk.Window):
     def __init__(self, app):
@@ -58,7 +67,6 @@ class MainWindow(Gtk.Window):
 #        self.tid = GLib.timeout_add(100, self.update_levels)
 
         bus = app.pipeline.get_bus()
-        bus.add_signal_watch()
         bus.connect("message::element", self.bus_element_cb)
 
         self.toggle.connect('clicked', self.app.toggle)
@@ -70,16 +78,21 @@ class MainWindow(Gtk.Window):
         idx = self.app.levels.index(msg.src)
         peak = s.get_value('peak')[0]
         self.bars[idx].set_fraction( 1.0 + peak/20.0 )
+#        if idx == 0:
+#            print 'PEAK ', s.get_value('peak')[0] , ' AVG RMS ', s.get_value('rms')[0]
+
 
 #    def update_levels(self):
 #        return True
 
     def slider_cb(self, slider, chan):
         self.app.set_channel_volume (chan, slider.get_value())
-        self.app.set_active_input(chan)
 
 class App(object):
     def __init__(self):
+        self.current_input = 0
+        self.last_switch_time = time.time()
+
         self.pipeline = pipeline = Gst.Pipeline.new('pipeline')
 
         self.inputsel = Gst.ElementFactory.make ('input-selector', None)
@@ -105,6 +118,8 @@ class App(object):
         self.audio_inputs = []
         self.audio_queues = []
         self.audio_tees = []
+        self.audio_avg = []
+        self.audio_peak = []
         self.fasinks = []
 
         self.video_inputs = []
@@ -133,11 +148,15 @@ class App(object):
             freq = 440*(idx+1)
             self.add_audio_source(props=[('freq', freq), ('is-live', True)])
 
-        for idx,pad in enumerate(self.vmixer.sinkpads):
-            pad.set_property('ypos' , 0)
-            pad.set_property('xpos' , 320*idx)
+#        for idx,pad in enumerate(self.vmixer.sinkpads):
+#            pad.set_property('ypos' , 0)
+#            pad.set_property('xpos' , 320*idx)
 
     def add_audio_source (self, sourcename=None, props=None):
+        # 10 samples per second
+        self.audio_avg.append (deque (maxlen=WINDOW_LENGTH * 10))
+        self.audio_peak.append (deque (maxlen=WINDOW_LENGTH * 10))
+
         name = sourcename or 'audiotestsrc'
         src = Gst.ElementFactory.make (name, None)
         q0 = Gst.ElementFactory.make ('queue2', None)
@@ -221,6 +240,7 @@ class App(object):
         idx = 1 + (inputidx % (len(isel.pads)-1))
 
         newpad = isel.pads[idx]
+        self.current_input = inputidx
         if idx != isel.pads.index(oldpad):
             isel.set_property('active-pad', newpad)
 
@@ -228,16 +248,72 @@ class App(object):
         e = self.inputsel
         s = e.get_property ('active-pad')
         # pads[0] output, rest input sinks.
+        # set_active_input() uses 0..N, so this works out to switch to the next
         i = e.pads.index(s)
         self.set_active_input(i)
+
+    def start (self):
+        self.pipeline.set_state (Gst.State.PLAYING)
+        bus = self.pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message::element", self.bus_element_cb)
+        self.tid = GLib.timeout_add(int (UPDATE_INTERVAL * 1000), self.process_levels)
+
+# XXX: devolver True, sino el timeout se destruye
+    def process_levels (self):
+        now = time.time()
+        def do_switch (src):
+            if src == self.current_input:
+                return
+            self.last_switch_time = now
+            self.set_active_input (src)
+            print 'DO_SWITCH ', src
+
+        if (now - self.last_switch_time) < MIN_ON_AIR_TIME:
+            return True
+        print 'PROCESS current_input ', self.current_input
+        dpeaks = []
+        avgs = []
+        for idx,q in enumerate (self.audio_avg):
+            avgs.append ( (idx, sum (q) / (10*WINDOW_LENGTH)) )
+
+        for idx,q in enumerate (self.audio_avg):
+            dp = []
+            for (x1,x2) in zip (q, list(q)[1:]):
+                dp.append (x2-x1)
+            dpeaks.append ( (idx, sum(dp) / (10*(WINDOW_LENGTH-1))) )
+
+# ver caso si mas de uno pasa umbral.
+        peaks_over = filter (lambda x: x[1] > SPEAK_UP_THRESHOLD, dpeaks)
+        if peaks_over:
+            idx, peak = max (peaks_over, key= lambda x: x[1])
+            print ' PEAKS OVER ', peaks_over
+            do_switch (idx)
+            return True
+
+        idx, avg = max (avgs, key= lambda x: x[1])
+        do_switch (idx)
+        #return True
+
+        print ' AVGs ', avgs , ' dPEAKs ', dpeaks
+        return True
+
+    def bus_element_cb (self, bus, msg):
+        s = msg.get_structure()
+        if s.get_name() != "level":
+            return
+        idx = self.levels.index (msg.src)
+        self.audio_avg[idx].append (s.get_value ('rms')[0])
+        self.audio_peak[idx].append (s.get_value ('peak')[0])
 
 if __name__ == "__main__":
 
     app = App()
 
-    app.pipeline.set_state (Gst.State.PLAYING)
     w2 = MainWindow(app)
     w2.show_all()
+
+    app.start()
 
     Gst.debug_bin_to_dot_file(app.pipeline, Gst.DebugGraphDetails.MEDIA_TYPE | Gst.DebugGraphDetails.NON_DEFAULT_PARAMS , 'debug1')
     Gst.debug_bin_to_dot_file(app.pipeline, Gst.DebugGraphDetails.MEDIA_TYPE | Gst.DebugGraphDetails.NON_DEFAULT_PARAMS | Gst.DebugGraphDetails.CAPS_DETAILS, 'debug1')
