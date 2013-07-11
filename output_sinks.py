1f7cf6224aea4f05fd903f9435915b62ead4056a
Adrian Pardini <publico@tangopardo.com.ar>
2013-07-11 09:58:38 -0300

    fixes "DTS method failed to re-order timestamps"

--- output_sinks.py
+++ output_sinks.py
@@ -64,6 +64,8 @@ class MP4Output(Gst.Bin):
 
         aq = Gst.ElementFactory.make('queue2', 'audio q')
         vq = Gst.ElementFactory.make('queue2', 'video q')
+        vmuxoq = Gst.ElementFactory.make('queue2', 'video mux out q')
+        vmuxviq = Gst.ElementFactory.make('queue2', 'video mux video in q')
         self.preview_sink = None
 
         aenc = Gst.ElementFactory.make('avenc_aac', None)
@@ -73,20 +75,40 @@ class MP4Output(Gst.Bin):
         vsink.set_property('port', 9078)
         vmux = Gst.ElementFactory.make ('mp4mux', None)
         vmux.set_property('streamable', True)
-        vmux.set_property('fragment-duration', 100)
+        vmux.set_property('fragment-duration', 1000)
+
+        # the default (reorder) gives a nicer and faster a/v sync
+        # but x264 produces frames with  DTS timestamp and that doesn't need
+        # to be always increasing. So mp4mux is not happy, see gstqtmux.c around
+        # line 2800.
         vmux.set_property('dts-method', 2) # ascending
 
+        vmux.set_property('streamable', True)
 
         parser = Gst.ElementFactory.make ('h264parse', None)
         parser.set_property ('config-interval',2)
 
         venc = Gst.ElementFactory.make ('x264enc', None)
+        venc.set_property('byte-stream', True)
         venc.set_property('tune', 'zerolatency')
         venc.set_property('speed-preset', 'ultrafast')
-        venc.set_property ('key-int-max',15)
+        # while it might be usefull to avoid showing artifacts for a while
+        # touching it plays havoc with mp4mux and the default reorder method.
+        # https://bugzilla.gnome.org/show_bug.cgi?id=631855
+        # Just saying, so I don't forget it in the future.
+        # It lowers the compression ratio but gives a stable image faster.
+        venc.set_property ('key-int-max',30)
+
+        # these two are (supposedly) needed to avoid getting out of order
+        # timestamps. We need them even if we don't use the reorder method
+        # because otherwise the audio will lag.
+        # (it still does a little but is hard to notice).
+        venc.set_property('b-adapt', False)
+        venc.set_property('bframes', 0)
+
         venc.set_property ('bitrate',1024)
 
-        for el in [aq, vq, aenc, venc, parser, vmux, vsink]:
+        for el in [aq, vq, aenc, venc, parser, vmux, vmuxoq, vmuxviq, vsink]:
             self.add(el)
 
         aq.link(aenc)
@@ -96,8 +118,10 @@ class MP4Output(Gst.Bin):
         vq.link_filtered(venc, caps)
 
         venc.link(parser)
-        parser.link(vmux)
-        vmux.link(vsink)
+        parser.link(vmuxviq)
+        vmuxviq.link(vmux)
+        vmux.link(vmuxoq)
+        vmuxoq.link(vsink)
 
 
         agpad = Gst.GhostPad.new('audiosink', aq.get_static_pad('sink'))
