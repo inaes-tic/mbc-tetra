8174f8de2a28901d90b263a8b8c01bcb438c60ea
Adrian Pardini <publico@tangopardo.com.ar>
2013-07-14 14:12:49 -0300

    config: save local changes upon exiting.

--- config.py
+++ config.py
@@ -18,6 +18,15 @@ except ValueError:
 # could not decode json, file empty?
     pass
 
+
+import atexit
+@atexit.register
+def __save_config():
+    fp = open('config.json', 'r+')
+    json.dump(__defaults, fp, indent=4, sort_keys=True)
+    fp.close()
+
+
 __defaults.update(__user)
 
 config = __defaults
