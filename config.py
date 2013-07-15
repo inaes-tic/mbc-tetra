0553c4ac734151bf65c1da283fe5602e88509e58
Adrian Pardini <publico@tangopardo.com.ar>
2013-07-14 23:25:28 -0300

    config: better error handling, do not leave previous contents at save

--- config.py
+++ config.py
@@ -5,24 +5,26 @@ __defaults = {}
 try:
     fp = open('config.json', 'r+')
     __user = json.load(fp)
-    fp.close()
 except ValueError:
 # could not decode json, file empty?
     pass
+finally:
+    fp.close()
 
 try:
     fp = open('defaults.json', 'r+')
     __defaults = json.load(fp)
-    fp.close()
 except ValueError:
 # could not decode json, file empty?
     pass
+finally:
+    fp.close()
 
 
 import atexit
 @atexit.register
 def __save_config():
-    fp = open('config.json', 'r+')
+    fp = open('config.json', 'w')
     json.dump(__defaults, fp, indent=4, sort_keys=True)
     fp.close()
 
@@ -30,4 +32,4 @@ def __save_config():
 __defaults.update(__user)
 
 config = __defaults
-get = config.get
+get = config.setdefault
