6bd87f6ccfdc897972af03f131e1ecd5c65d9a5b
Adrian Pardini <publico@tangopardo.com.ar>
2013-07-19 00:14:30 -0300

    adds file output sink

--- output_sinks.py
+++ output_sinks.py
@@ -202,3 +202,51 @@ class FLVOutput(Gst.Bin):
     def initialize(self):
         pass
 
+class StreamWriter(Gst.Bin):
+    def __init__(self, name=None, location='/dev/null', append=False):
+        Gst.Bin.__init__(self)
+        if name:
+            self.set_property('name', name)
+        self._saving = True
+
+        valve = Gst.ElementFactory.make('valve', None)
+        q = Gst.ElementFactory.make('queue2', None)
+        fsink = Gst.ElementFactory.make('filesink', None)
+
+        for el in [valve, q, fsink]:
+            self.add(el)
+
+        valve.link(q)
+        q.link(fsink)
+
+        fsink.set_property('append', append)
+        fsink.set_property('location', location)
+        valve.set_property('drop', False)
+
+        self.valve = valve
+        self.q = q
+        self.fsink = fsink
+
+        sinkpad = Gst.GhostPad.new('sink', self.valve.get_static_pad('sink'))
+        self.add_pad(sinkpad)
+
+    def stop(self, done=None):
+        self._saving = False
+
+        self.valve.set_property('drop', True)
+        self.q.set_state(Gst.State.NULL)
+        self.fsink.set_state(Gst.State.NULL)
+
+    def start(self, location=None):
+        if self._saving:
+            self.stop()
+
+        self._saving = True
+
+        if location is not None:
+            self.fsink.set_property('location', location)
+
+        self.valve.set_property('drop', False)
+        self.q.set_state(Gst.State.PLAYING)
+        self.fsink.set_state(Gst.State.PLAYING)
+
